67020917140e55f733fc6261978d4b188a3095fe
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2023-12-02 11:02:22 +0100

    Day 2b

--- day02.go
+++ day02.go
@@ -35,6 +35,41 @@ func day2aWork(lines []string) int {
 }
 
 func day2a() int {
-	input := mustReadInput(2)
-	return day2aWork(input)
+	return day2aWork(mustReadInput(2))
+}
+
+func day2bPower(line string) int {
+	var minRed, minGreen, minBlue int
+
+	_, rest, _ := strings.Cut(line, ": ")
+	for _, set := range strings.Split(rest, "; ") {
+		for _, item := range strings.Split(set, ", ") {
+			var count int
+			var color string
+			fmt.Sscanf(item, "%d %s", &count, &color)
+
+			switch {
+			case color == "red" && count > minRed:
+				minRed = count
+			case color == "green" && count > minGreen:
+				minGreen = count
+			case color == "blue" && count > minBlue:
+				minBlue = count
+			}
+		}
+	}
+
+	return minRed * minGreen * minBlue
+}
+
+func day2bWork(lines []string) int {
+	var total int
+	for _, line := range lines {
+		total += day2bPower(line)
+	}
+	return total
+}
+
+func day2b() int {
+	return day2bWork(mustReadInput(2))
 }
--- day02_test.go
+++ day02_test.go
@@ -40,3 +40,42 @@ func TestDay2a(t *testing.T) {
 	result := day2a()
 	AssertEqual(t, expect, result)
 }
+
+func TestDay2bPower(t *testing.T) {
+	tt := []struct {
+		input  string
+		expect int
+	}{
+		{"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48},
+		{"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12},
+		{"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 1560},
+		{"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 630},
+		{"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36},
+	}
+
+	for _, tc := range tt {
+		t.Run("", func(t *testing.T) {
+			result := day2bPower(tc.input)
+			AssertEqual(t, tc.expect, result)
+		})
+	}
+}
+
+func TestDay2bWork(t *testing.T) {
+	input := []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
+	expect := 2286
+	result := day2bWork(input)
+	AssertEqual(t, expect, result)
+}
+
+func TestDay2b(t *testing.T) {
+	expect := 72970
+	result := day2b()
+	AssertEqual(t, expect, result)
+}
